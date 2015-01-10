1d9c10036c224c79f24714f798f964b71df2e01d
Tom Leonardsson <tom.leo@hotmail.com>
2015-01-10 12:20:41 +0100

    No need for capital letter and get; set; if private

--- FinalSpelProject/FinalSpelProject/FinalSpelProject/Enemy.cs
+++ FinalSpelProject/FinalSpelProject/FinalSpelProject/Enemy.cs
@@ -14,50 +14,50 @@ namespace FinalSpelProject
 {
     class Enemy : GameObject
     {
-        sbyte Type {get; set;}
-        short Health { get; set; }
-        short Armor { get; set; }
-        float FireRate { get; set; }
+        sbyte type; 
+        short health;
+        short armor; 
+        float fireRate;
 
         public Enemy(sbyte type2)
         {
-            Type = type2;
-            switch (Type)
+            type = type2;
+            switch (type)
             {
                 //Examples, no real purpose but to test.
                 // case 11 idicates enemyType 1 on lvl 1.
                 case 11:
-                    Health = 2;
-                    FireRate = 30;
+                    health = 2;
+                    fireRate = 30;
                     break;
 
                 case 12:
-                    Health = 4;
-                    FireRate = 30;
+                    health = 4;
+                    fireRate = 30;
                     break;
                 //13 spawns and starts going towards the player, kamikaze style.
                 case 13:
                     SetSpriteCoords(1, 1);
                     SetSize(32);
                     AnimationActive = true;
-                    Health = 5;
-                    Armor = 5;
+                    health = 5;
+                    armor = 5;
                     VelX = 5;
                     VelY = 5;
                     Speed = 2.5f;
                     break;
 
                 case 14:
-                    Health = 10;
-                    Armor = 10;
-                    FireRate = 60;
+                    health = 10;
+                    armor = 10;
+                    fireRate = 60;
                     break;
             }
         }
 
         public void Update(List<Player> player)
         {
-            switch (Type)
+            switch (type)
             {
                     //Examples, no real purpose but to test.
                     // case 11 idicates enemyType 1 on lvl 1.

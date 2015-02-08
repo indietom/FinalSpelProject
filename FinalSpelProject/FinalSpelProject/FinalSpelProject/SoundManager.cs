1d844a119636ced45583c6aded51335bf895e887
Tom Leonardsson <tom.leo@hotmail.com>
2015-02-08 20:44:02 +0100

    Sound manager class

---
+++ FinalSpelProject/FinalSpelProject/FinalSpelProject/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+namespace FinalSpelProject
+{
+    class SoundManager
+    {
+        public static bool SoundOff;
+
+        public static SoundEffect
+            NormalShot,
+            Explosion;
+
+        public static void PlaySound(SoundEffect soundEffect)
+        {
+            if (!SoundOff) soundEffect.Play(); 
+        }
+
+        public static void Load(ContentManager content)
+        {
+
+        }
+    }
+}

02dfa2fe853f595a16b042b946f6ed78967b8026
Tom Leonardsson <tom.leo@hotmail.com>
2015-02-17 21:19:55 +0100

    Spawnmanager, globals class and some functions
    bud på octapus class :^))))))))))

---
+++ FinalSpelProject/FinalSpelProject/FinalSpelProject/SpawnManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace FinalSpelProject
+{
+    class SpawnManager
+    {
+        short[] spawnEnemyTimers = new short[Globals.AmountOfEnemyTypes];
+        short[] MaxSpawnEnemyTimers = new short[Globals.AmountOfEnemyTypes];
+
+        public void Update()
+        {
+
+        }
+
+        public void PowerUpSpawnUpdate()
+        {
+
+        }
+
+        public void EnemySpawnUpdate()
+        {
+ 
+        }
+
+        public void SpawnEnemiesLine(Vector2 pos2, byte height, byte type, List<Enemy> enemies)
+        {
+
+        }
+
+        public void SpawnEnemiesTriangle(Vector2 pos2, byte width, byte height, byte type, byte enemySize, List<Enemy> enemies)
+        {
+            Random random = new Random();
+            for (int i = 0; i < height; i++ )
+            {
+                enemies.Add(new Enemy(new Vector2(pos2.X + (i + 1) * enemySize, pos2.Y - i * enemySize), type, random));
+                enemies.Add(new Enemy(new Vector2(pos2.X - (i - 1) * enemySize, pos2.Y - i * enemySize), type, random));
+            }
+        }
+        public void SpawnEnemiesTriangle(Vector2 pos2, byte width, byte height, byte type, List<Enemy> enemies)
+        {
+            SpawnEnemiesTriangle(pos2, width, height, type, 64, enemies);
+        }
+    }
+}

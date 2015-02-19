c3cff0312e51ccb5060fe841688174cce26c4412
Tom Leonardsson <tom.leo@hotmail.com>
2015-02-19 19:33:31 +0100

    ProceduralGenerationManager class

---
+++ FinalSpelProject/FinalSpelProject/FinalSpelProject/ProceduralGenerationManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalSpelProject
+{
+    class ProceduralGenerationManager
+    {
+        byte roadDirection;
+        byte lakeSize;
+
+        short roadSpawnCount;
+        short maxRoadSpawnCount;
+        short lakeSpawnCount;
+        short maxLakeSpawnCount;
+
+        bool roadDirectionChange;
+        bool spawningRoads;
+        bool spawningLakes;
+
+        public void Update()
+        {
+
+        }
+        
+        public void SpawnRoad()
+        {
+
+        }
+
+        public void SpawnLakes()
+        {
+
+        }
+    }
+}

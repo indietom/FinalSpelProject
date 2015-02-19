e73e7022a4b8f9cb8512fce0c38118c579b8e30f
Tom Leonardsson <tom.leo@hotmail.com>
2015-02-19 22:19:40 +0100

    Road spawn functions

--- FinalSpelProject/FinalSpelProject/FinalSpelProject/ProceduralGenerationManager.cs
+++ FinalSpelProject/FinalSpelProject/FinalSpelProject/ProceduralGenerationManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace FinalSpelProject
 {
@@ -19,6 +20,11 @@ namespace FinalSpelProject
         bool spawningRoads;
         bool spawningLakes;
 
+        public ProceduralGenerationManager()
+        {
+
+        }
+
         public void Update()
         {
 
@@ -32,6 +38,51 @@ namespace FinalSpelProject
         public void SpawnLakes()
         {
 
+        }
+
+        // hopefully this will make the actual procedural-generation algorithm more readable 
+        public void RoadTurn(Vector2 pos2, bool left, List<Tile> tiles)
+        {
+            Point turnTile = new Point(0, 0);
+            if (left)
+                turnTile = new Point(0, 1);
+            else
+                turnTile = new Point(2, 1);
+            for(int y = 0; y < 3; y++)
+            {
+                for(int x = 0; x < 3; x++)
+                {
+                    if (new Point(x, y) != turnTile && new Point(1, 1) != new Point(x, y) && new Point(1, 2) != new Point(x, y))
+                        tiles.Add(new Tile(pos2 + new Vector2(x * 16, y * 16), 2));
+                    if (new Point(x, y) == turnTile)
+                        tiles.Add(new Tile(pos2 + new Vector2(x * 16, y * 16), 4));
+                    if (new Point(1, 1) == new Point(x, y))
+                        tiles.Add(new Tile(pos2 + new Vector2(x * 16, y * 16), 5));
+                    if (new Point(1, 2) == new Point(x, y))
+                        tiles.Add(new Tile(pos2 + new Vector2(x * 16, y * 16), 3));
+                }
+            }
+        }
+
+        public void SimpelRoad(Vector2 pos2, bool vertical, List<Tile> tiles)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if (vertical)
+                {
+                    if (i != 1)
+                        tiles.Add(new Tile(pos2 + new Vector2(0, i * 16), 2));
+                    else
+                        tiles.Add(new Tile(pos2 + new Vector2(0, i * 16), 4));
+                }
+                else
+                {
+                    if (i != 1)
+                        tiles.Add(new Tile(pos2 + new Vector2(i * 16, 0), 2));
+                    else
+                        tiles.Add(new Tile(pos2 + new Vector2(i * 16, 0), 3));
+                }
+            }
         }
     }
 }

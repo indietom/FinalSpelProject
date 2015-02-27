a3f7e7b337d4de24811f297070aeb3e6f44098fb
Tom Leonardsson <tom.leo@hotmail.com>
2015-02-27 23:04:52 +0100

    TextBox class

---
+++ FinalSpelProject/FinalSpelProject/FinalSpelProject/TextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FinalSpelProject
+{
+    class TextBox : GameObject
+    {
+        string fullText;
+        string visualText;
+
+        byte addLetterCount;
+        byte maxAddLetterCount;
+
+        int currentLetter;
+
+        public TextBox(Vector2 pos2, string text2, Color color2, byte maxAddLetterCount2)
+        {
+            Pos = pos2;
+            fullText = text2;
+            color = color2;
+            OrginalColor = color2;
+            maxAddLetterCount = maxAddLetterCount2;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D spritesheet, SpriteFont font)
+        {
+            
+        }
+    }
+}

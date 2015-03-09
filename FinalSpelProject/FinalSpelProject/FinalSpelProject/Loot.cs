67c059e4c68d954680650199942d11d605a3c19b
Tom Leonardsson <tom.leo@hotmail.com>
2015-03-09 21:26:13 +0100

    Worked on the loot class

--- FinalSpelProject/FinalSpelProject/FinalSpelProject/Loot.cs
+++ FinalSpelProject/FinalSpelProject/FinalSpelProject/Loot.cs
@@ -9,12 +9,33 @@ namespace FinalSpelProject
 {
     class Loot : GameObject
     {
-        
-    }
-
-    struct LootProperty
-    {
         short worth;
+
+        byte movmentType;
+        byte type;
+
+        public Loot(Vector2 pos2, byte type2, byte movmentType2)
+        {
+            Pos = pos2;
+            type = type2;
+            movmentType = movmentType2;
+            SetSpriteCoords((short)(780 + FrameX(type)), 69);
+            SetSize(16);
+            worth = GetWorth();
+        }
         
+        public short GetWorth()
+        {
+            short[] worths = new short[6];
+            
+            worths[0] = 1000;
+            worths[1] = 500;
+            worths[2] = 1500;
+            worths[3] = 1250;
+            worths[4] = 900;
+            worths[5] = 2000;
+
+            return worths[type];
+        }
     }
 }
